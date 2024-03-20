1baf047a21bc0eaf00d22604c92e6c1ac1e9a127
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2024-03-20 15:23:50 -0300

    2024/03/20 - Capítulo 04

---
+++ 04/Exempos/Ex02/Course.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace 04.Exempos.Ex02
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}
---
+++ 04/Exercicios/Ex01/Pessoa.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace 04.Exercicios.Ex01
+{
+    public class Pessoa
+    {
+        public string Nome;
+        public int Idade;  
+    }
+}
