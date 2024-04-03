d55a3fed4c78c0c66b4edb8ffe43518a285d41c5
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2024-04-03 16:19:24 -0300

    2024-04-03 - Capítulo 11 - Exemplo 02.

---
+++ 11/Exemplo/02/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace 02.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
